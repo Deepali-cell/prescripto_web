6921bea874e9781999d378a675e4352057f43e53
Deepali <joysingal247@gmail.com>
2025-08-20 10:49:28 +0530

    changes in backend

--- backend/index.js
+++ backend/index.js
@@ -5,42 +5,30 @@ import connectDB from "./config/mongodb.js";
 import adminRouter from "./routes/adminRouter.js";
 import doctorRouter from "./routes/doctorRouter.js";
 import UserRouter from "./routes/userRouter.js";
-import path from "path";
 
 // app config
 const port = process.env.PORT || 3000;
 const app = express();
 connectDB();
 
-const __dirname = path.resolve();
-
 // middleware
 app.use(express.json());
-const coreOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"], // frontend apps
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173", // dev frontend
+    "http://localhost:5174", // dev admin
+    "https://prescripto-frontend.onrender.com", // deployed frontend
+    "https://prescripto-admin.onrender.com", // deployed admin panel
+  ],
   credentials: true,
 };
-app.use(cors(coreOptions));
+app.use(cors(corsOptions));
 
 // api endpoints
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/user", UserRouter);
-
-// Serve main frontend
-app.use(express.static(path.join(__dirname, "frontend/dist")));
-app.get("/", (_, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-app.get("/user/*", (_, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
-// Serve admin panel frontend
-app.use("/admin", express.static(path.join(__dirname, "admin-panel/dist")));
-app.get("/admin/*", (_, res) => {
-  res.sendFile(path.join(__dirname, "admin-panel", "dist", "index.html"));
-});
 
 // Start server
 app.listen(port, () => {
